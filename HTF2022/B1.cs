a5007a75b864d84475e9283706c1e679a2068b0f
Mohammed <mohammedsouleymane@outlook.com>
2022-11-15 07:46:20 +0100

    B1

--- HTF2022/B1.cs
+++ HTF2022/B1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,9 @@ namespace HTF2022
 
         internal static void LocalExecution()
         {
+            var list = new List<string>(){"test","string"};
+            var chars = GetCharacterAndCount(list);
+            Console.WriteLine(GetWord(chars));
             Console.WriteLine("-Local Execution: \n");
            
         }
@@ -32,6 +36,30 @@ namespace HTF2022
         {
             Console.WriteLine("-Production Execution: \n");
            
+        }
+
+        static string? GetWord(List<IGrouping<char, char>> chars)
+        {
+            var word = new char[chars.Max(x => x.Count())];
+            foreach (var character in chars)
+            {
+                word[character.Count() - 1] = character.Key;
+            }
+            return new string(word);
+        }
+
+        static List<IGrouping<char, char>> GetCharacterAndCount(List<string> strings)
+        {
+            var chars = new List<IGrouping<char, char>>();
+            foreach (var VARIABLE in strings)
+            {
+                var perChar = VARIABLE.ToCharArray().GroupBy(x => x);
+                foreach (var unique in perChar)
+                {
+                    chars.Add(unique);
+                }
+            }
+            return chars;
         }
     }
 }
